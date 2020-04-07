4aa9514e6bba3e3f6b972effd8e393d0975d9323
EJ-C <eun530k@gmail.com>
2020-04-07 13:00:58 +0900

    integrated firebase into application, created sign in page with sign in component and sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCB_jVY-Xum1k8xTO8zSUxXmmJVGeGZLQc",
+    authDomain: "ecommerce-ej-db.firebaseapp.com",
+    databaseURL: "https://ecommerce-ej-db.firebaseio.com",
+    projectId: "ecommerce-ej-db",
+    storageBucket: "ecommerce-ej-db.appspot.com",
+    messagingSenderId: "185638683676",
+    appId: "1:185638683676:web:c70eebccff258dd56e8bd9",
+    measurementId: "G-HDZ4Z9FNYC"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
